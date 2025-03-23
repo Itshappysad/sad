a4498c09a72b8d533a0cc501bec0ffe74225a2f6
Itshappysad <martinez.andres1301@gmail.com>
2025-03-23 17:57:36 -0500

    :)

--- doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"model","l":"Barco"},{"p":"model","l":"Carga"},{"p":"model","l":"Cliente"},{"p":"ui","l":"MorganApp"},{"p":"model","l":"Naviera"},{"p":"model","l":"TipoCarga"},{"p":"model","l":"TipoCliente"},{"p":"model","l":"Validacion"}];updateSearchResults();
